c7ec12e6d19f5399a6c6858234bda321a34df735
jmuletr <jmuletr@gmail.com>
2017-02-24 12:31:28 +0100

    esquelet

---
+++ src/Printer.java
@@ -0,0 +1,18 @@
+/**
+ * Created by jmuletr on 23/02/17.
+ */
+public class Printer {
+    // Constructor: accepta un String amb la representació de tot l'alfabet
+// Hi haurà 27 lletres en total, separades per un espai en blanc.
+    public Printer(String alphabet) {
+    }
+
+    // Mètode render: accepta un String amb el text a representar i torna
+// també un String amb el text en forma d'ASCII Art.
+// Només es consideren les lletres majúscules de la A a la Z. Les
+// minúscules es passen a majúscules, i els altres caràctes es tradueixen
+// a «?» (manco l'espai en blanc).
+    public String render(String text) {
+        return null;
+    }
+}
