12916fbe2db65c49f0945153ec4ebff13e0634b1
jmuletr <jmuletr@gmail.com>
2017-02-24 13:16:11 +0100

    merda d'espais no se que fer amb ells

--- src/Printer.java
+++ src/Printer.java
@@ -2,9 +2,18 @@
  * Created by jmuletr on 23/02/17.
  */
 public class Printer {
+    private String alphabet;
+    private int counter;
     // Constructor: accepta un String amb la representació de tot l'alfabet
 // Hi haurà 27 lletres en total, separades per un espai en blanc.
     public Printer(String alphabet) {
+        this.alphabet = alphabet;
+        System.out.println(alphabet.length());
+        for (int i = 0; i < alphabet.length(); i++) {
+            if (alphabet.charAt(i) == '\n') counter++;
+
+        }
+        counter++;
     }
 
     // Mètode render: accepta un String amb el text a representar i torna
@@ -13,6 +22,33 @@ public class Printer {
 // minúscules es passen a majúscules, i els altres caràctes es tradueixen
 // a «?» (manco l'espai en blanc).
     public String render(String text) {
-        return null;
+        text = text.toUpperCase();
+        String result = "";
+        int newLine = alphabet.length()/ counter +1;
+        System.out.println(newLine);
+        int valChar;
+        int widthChar = ((alphabet.length()/27)/ counter + 1);
+        for (int i = 0, x = 0; i < counter; i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (text.charAt(j) > 64 && text.charAt(j) < 91) {
+                    valChar = (text.charAt(j) - 'A') * (widthChar);
+                    System.out.println(valChar);
+                    result += alphabet.substring(valChar+x, (valChar + widthChar)+x);
+                    System.out.println(alphabet.substring(valChar+x, (valChar + widthChar)+x));
+                }
+                else if(text.charAt(j) == 32){
+
+                    result+= " ";
+
+                }else{
+                    valChar = newLine - widthChar-1;
+                    result += alphabet.substring(valChar+x, (valChar +(widthChar-2))+x);
+                }
+            }
+            x += newLine;
+            if (i < counter - 1) result += "\n";
+        }
+        System.out.println(result);
+        return result;
     }
 }
